2af85f62f7ffed7417ac3c8ee32377789bbfab72
dbiv-39 <114115863+dbiv-39@users.noreply.github.com>
2023-01-12 18:59:20 -0600

    Added Dynamic deployment of commands

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@
 Rinabot updated to DiscordJS v14.
 Invoke with 'npm run start:dev'
 */
-import { config } from 'dotenv';
-import { Client, CommandInteractionOptionResolver, Message, RouteBases, Routes } from 'discord.js';
-import { REST } from '@discordjs/rest';
-
-//Loads in variables in .env file.
-config();
-
+require('dotenv').config();
+const {Client, Collection, Events, GatewayIntentBits, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const  fs  = require('fs');
+const  path  = require('path');
 
 
 const client = new Client({intents: ['Guilds', 'GuildMessages']});
+client.commands = new Collection();
+
+//Dynamically retrieve command files.
+const commands = [];
+const commandsPath = path.join(__dirname, '../commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    //Set new item in collection with the key as the command name and the vlaue as exported module.
+    if ('data' in command && 'execute' in command){
+        client.commands.set(command.data.name, command);
+        commands.push(command.data.toJSON());
+
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+
 const TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
@@ -22,13 +41,32 @@ client.on('ready', () => {
     console.log(`${client.user.tag} has logged in!`);
 });
 
-async function main(){
-    const commands = [
-        {
-            name: 'ping',
-            description: 'Echos Pong!',
+//Command listener which responds with proper command if exists, logs and ignores if nonexistent.
+client.on('interactionCreate', async interaction => {
+    if (interaction.isChatInputCommand()){
+
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command){
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
         }
-    ];
+        try{
+            await command.execute(interaction);
+        } catch (error){
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
+        }
+    }
+});
+
+/**
+ * main is a asynchronous function which updates commands supported by Rina, additionally it will also login the bot.
+ * 
+ */
+async function main(){
+
+    
 
     try{
         console.log("Started refreshing application (/) commands.");
@@ -42,7 +80,3 @@ async function main(){
 }
 
 main();
-
-//client.on('messageCreate.content', (message) => {
-    //console.log(message.content);
-//});
