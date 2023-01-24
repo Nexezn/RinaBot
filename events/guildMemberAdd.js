18af82a8b7b59dfdeb3a60df10f0860dd40255d2
dbiv-39 <114115863+dbiv-39@users.noreply.github.com>
2023-01-24 12:40:42 -0600

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,23 +1,47 @@
 const { Events, AttachmentBuilder } = require('discord.js');
+const { request } = require('undici');
 const Canvas = require('@napi-rs/canvas');
+
 
 module.exports = {
     name: Events.GuildMemberAdd,
+    once: false,
 
-    async execute(interaction){
-        //TODO:Greet user first in the bridge!
+    /**
+     * 
+     * @param {object} client 
+     */
+    async execute(client){
+
+        const welcomeChannel = member.guild.channels.get('989288360281526303');
 
         //TODO: Attach image of them.
         const canvas = Canvas.createCanvas(700, 250);
         const context = canvas.getContext('2d');
-        const background = await Canvas.loadImage('yimages/wallpaper.png');
-
-        //Stretch image onto the entire canvas
+        const background = await Canvas.loadImage('images/wallpaper.png');
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+        //TODO: REPLACE interaction.user with new user.
+        const { body } = await request(interaction.user.displayAvatarURL( { extension: 'jpg'}));
+        const avatar = await Canvas.loadImage(await body.arrayBuffer());
+
+
+        //Text for user
+        context.font = '28px sans-serif';
+
+        //DrawShapes with pen
+        context.beginPath();
+        context.arc(125, 125, 100, 0, Math.PI * 2, true);
+        context.closePath();
+        context.clip();
+
+        //Draw images onto the entire canvas
+        
+        context.drawImage(avatar, 25, 25, 200, 200, canvas.height);
 
         //Use attatchment class to process file
         const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-join-image.png' });
 
-        interaction.reply({ content: `Welcome ${interaction.client.user.tag} to HeartBeat Garage!`, files: [attachment]})
+        welcomeChannel.send({ content: `Welcome ${client.user.tag} to HeartBeat Garage!`, files: [attachment]});
     },
 };
