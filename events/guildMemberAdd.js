4b05835e16121100c245680ddd30a608c80a88c5
dbiv-39 <114115863+dbiv-39@users.noreply.github.com>
2023-01-28 13:12:32 -0600

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,10 @@
-const { Events, AttachmentBuilder } = require('discord.js');
+/* 
+Sends 
+*/
+const { Events, AttachmentBuilder, GuildMember } = require('discord.js');
 const { request } = require('undici');
 const Canvas = require('@napi-rs/canvas');
-
+const { applyText } = require('../utils/textUtil.js');
 
 module.exports = {
     name: Events.GuildMemberAdd,
@@ -9,25 +12,33 @@ module.exports = {
 
     /**
      * 
-     * @param {object} client 
+     * @param {object} member 
      */
-    async execute(client){
+    async execute(member){
 
-        const welcomeChannel = member.guild.channels.get('989288360281526303');
+        //Get welcome channel ID and set it.
+        const channelID = '989288360281526303';
+        const welcomeChannel = member.guild.channels.cache.get(channelID);
 
-        //TODO: Attach image of them.
+        //Create background of welcome image.
         const canvas = Canvas.createCanvas(700, 250);
         const context = canvas.getContext('2d');
         const background = await Canvas.loadImage('images/wallpaper.png');
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-        //TODO: REPLACE interaction.user with new user.
-        const { body } = await request(interaction.user.displayAvatarURL( { extension: 'jpg'}));
-        const avatar = await Canvas.loadImage(await body.arrayBuffer());
+        //Draw Black Rectangle for text to be visible.
+        context.fillStyle = '#000000';
+        context.fillRect( 250, 50, 425, 150);
 
-
-        //Text for user
-        context.font = '28px sans-serif';
+        //Settings for Welcome title.
+        context.font = '36px sans-serif';
+        context.fillStyle = '#ffffff';
+        context.fillText('Welcome', 380, 75);
+        
+        //Settings for new user name.
+        context.font = '60px sans-serif';
+        context.fillStyle = '#ffffff';
+        context.fillText(member.displayName, (canvas.width / 2), (canvas.height / 1.8));
 
         //DrawShapes with pen
         context.beginPath();
@@ -35,13 +46,14 @@ module.exports = {
         context.closePath();
         context.clip();
 
-        //Draw images onto the entire canvas
-        
-        context.drawImage(avatar, 25, 25, 200, 200, canvas.height);
+        //TODO: REPLACE interaction.user with new user.
+        const { body } = await request(member.client.user.displayAvatarURL( { extension: 'jpg'}));
+        const avatar = await Canvas.loadImage(await body.arrayBuffer());
+        context.drawImage(avatar, 25, 25, 200, 200);
 
         //Use attatchment class to process file
         const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-join-image.png' });
 
-        welcomeChannel.send({ content: `Welcome ${client.user.tag} to HeartBeat Garage!`, files: [attachment]});
+        welcomeChannel.send({ content: `Welcome ${member.client.user.tag} to HeartBeat Garage!`, files: [attachment]});
     },
 };
